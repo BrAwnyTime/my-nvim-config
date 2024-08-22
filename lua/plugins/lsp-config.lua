a52c24e806684f20ea06a01e1634f9d71e788e94
Brad Aune <bradaune@gmail.com>
2024-08-21 23:06:11 -0400

    adds none-ls for formatting and linting

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -170,6 +170,9 @@ return {
         -- capabilities = {},
         settings = {
           Lua = {
+            format = {
+              enable = false,
+            },
             completion = {
               callSnippet = 'Replace',
             },
@@ -193,6 +196,8 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
+      'prettier', -- Used to format Typescript and Javascript
+      'eslint_d',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
